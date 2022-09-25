45aef1e0af0ce72756f74bc17c2ca141c01df492
SAURABH08PANDEY <80577220+SAURABH08PANDEY@users.noreply.github.com>
2022-09-25 15:11:19 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,173 @@
+function getSadInterval() {
+    return Date.now() + 2000;
+    
+}
+
+function getGoneInterval() {
+    return Date.now() + Math.floor(Math.random() * 10000) + 2000;
+}
+
+function getHungryInterval() {
+    return Date.now() + /* Math.floor(Math.random() * 3000) +  */2000;
+    
+}
+
+function getKingStatus() {
+    return Math.random() > .9;
+}
+
+let score = 0;
+
+const moles = [
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-1')
+    },
+
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-2')
+    },
+
+
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-3')
+    },
+
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-4')
+    },
+
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-5')
+    },
+
+    {
+        status: "sad",
+        next: getSadInterval(),
+        king: false,
+        node: document.getElementById('hole-6')
+    },
+];
+
+function feed(event) {
+    if (event.target.tagName !== 'IMG' || !event.target.classList.contains('hungry')) {
+        return;
+    }
+
+    const mole = moles[parseInt(event.target.dataset.index)];
+
+    mole.status = "fed";
+    mole.next = getSadInterval();
+
+    if (mole.king) {
+        score += 2;
+        mole.node.children[0].src = 'mole-game/king-mole-fed.png';
+    }
+    else {
+        score++;  
+        mole.node.children[0].src = 'mole-game/mole-fed.png';
+
+    }
+    mole.node.children[0].classList.remove('hungry');
+
+    document.querySelector('.worm-container').style.width = `${score * 5}%`;
+
+    if (score >= 20) {
+        win();
+    }
+
+
+}
+
+function win() {
+    document.querySelector('.bg').classList.add("gone");
+    document.querySelector('.win').classList.remove("gone");
+}
+
+
+function getNextStatus(mole) {
+    
+    switch (mole.status) {
+        case "sad":
+        case "fed":
+            mole.next = getSadInterval();
+            mole.status = "leaving";
+            if(mole.king)
+                mole.node.children[0].src = 'mole-game/king-mole-leaving.png';
+            else
+                mole.node.children[0].src = 'mole-game/mole-leaving.png';
+
+            break;
+        case "leaving":
+            mole.next = getGoneInterval();
+            mole.status = "gone";
+            mole.node.children[0].classList.add("gone");
+            break;
+        
+        case "gone":
+            mole.status = 'hungry';
+            mole.king = getKingStatus();
+            mole.next = getHungryInterval();
+            mole.node.children[0].classList.add("hungry");
+            mole.node.children[0].classList.remove("gone");
+            if (mole.king) {
+                mole.node.children[0].src = 'mole-game/king-mole-hungry.png';
+            }
+            else {
+                mole.node.children[0].src = 'mole-game/mole-hungry.png';
+            }
+            
+            break;
+       
+        case "hungry":
+            mole.status = 'sad';
+            mole.next = getSadInterval();
+            mole.node.children[0].classList.remove("hungry");
+            if(mole.king)
+                mole.node.children[0].src = 'mole-game/king-mole-sad.png';
+            else
+                mole.node.children[0].src = 'mole-game/mole-sad.png';
+            break;
+        
+
+    }
+}
+
+
+let runAgainAt = Date.now() + 1000;
+
+function nextFrame() {
+    const now = Date.now();
+
+    if (runAgainAt <= now) {
+        for (let i = 0; i < moles.length; i++){
+            if (moles[i].next <= now) {
+                getNextStatus(moles[i]);
+
+            }
+        }
+        
+        runAgainAt = Date.now() + 100;
+    }
+    
+    requestAnimationFrame(nextFrame);
+}
+
+document.querySelector('.bg').addEventListener('click', feed);
+
+
+nextFrame();
